26a0db4619373cae627febbd65acba904d938525
dikshya-bhandari <dikshyabhandari05@gmail.com>
2021-06-02 16:51:52 +0545

    mynavbar

---
+++ script.js
@@ -0,0 +1,16 @@
+const bar =document.getElementById("btn");
+function changeMe(){
+    if(bar.style.display == "none"){
+        bar.style.display ="block";
+    
+    }else{
+        bar.style.display= "none";
+    }
+        
+    }
+
+
+
+// function changeMe(){
+//     bar.classList.toggle("my-links")
+// }
